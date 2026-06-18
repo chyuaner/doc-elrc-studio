0da6be8a844f4faacd02a4652540ddbc8bfea78c
Yuan Chiu <chyuaner@gmail.com>
2026-06-18 22:17:50 +0800

    feat: dark mode 切換動畫

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
-import { Theme } from 'vitepress'
+import { Theme, useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import { watch, onMounted } from 'vue'
+import { watch, onMounted, nextTick, provide } from 'vue'
 import { useRoute, useRouter } from 'vitepress'
 import './custom.css'
 
@@ -9,9 +9,43 @@ export default {
   setup() {
     const route = useRoute()
     const router = useRouter()
+    const { isDark } = useData()
     let activeSidebarObserver: IntersectionObserver | null = null
 
     if (typeof window !== 'undefined') {
+      provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+        const isAppearanceTransition = (document as any).startViewTransition
+          && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+        if (!isAppearanceTransition) {
+          isDark.value = !isDark.value
+          return
+        }
+
+        const clipPath = [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${Math.hypot(
+            Math.max(x, innerWidth - x),
+            Math.max(y, innerHeight - y)
+          )}px at ${x}px ${y}px)`
+        ]
+
+        await (document as any).startViewTransition(async () => {
+          isDark.value = !isDark.value
+          await nextTick()
+        }).ready
+
+        document.documentElement.animate(
+          { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+          {
+            duration: 300,
+            easing: 'ease-in',
+            fill: 'forwards',
+            pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+          }
+        )
+      })
+
       let isTransitioning = false
       const normalizePath = (path: string) => {
         return decodeURIComponent(path.split('#')[0].split('?')[0])
